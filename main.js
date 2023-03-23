6174f1f29b4d5200ae9d8e8c7507b44db62b28bf
Joe P Shoulak <joe@squarecompass.io>
2023-03-23 14:40:13 -0500

    pausing

--- main.js
+++ main.js
@@ -2,35 +2,28 @@ const { app, Tray, Menu, globalShortcut } = require("electron");
 
 let tray = null;
 
+const radio = (label, checked = false) => ({ label, checked, type: "radio" });
+
 app.whenReady().then(() => {
   tray = new Tray("./assets/icons/monitor.ico");
 
   const contextMenu = Menu.buildFromTemplate([
-    { label: "vdName1", type: "radio" },
-    { label: "vdName2", type: "radio" },
-    { label: "vdName3", type: "radio", checked: true },
-    { label: "vdName4", type: "radio" },
+    radio("vdName1"),
+    radio("vdName2"),
+    radio("vdName3"),
+    radio("vdName4", true),
     { type: "separator" },
     { label: "Exit", role: "quit" },
   ]);
 
-  globalShortcut.register("Control+Command+Tab", () => {
-    tray.displayBalloon({
-      title: "Display",
-      content: "You are on display vdNameCurrent",
-    });
-  });
+  const notifBalloon = {
+    title: "Display",
+    content: "You are on display vdNameCurrent",
+  };
 
-  const winapi = require("winapi");
-
-  winapi.desktop
-    .getCurrent()
-    .then(desktop => {
-      console.log(`Current virtual desktop: ${desktop.name}`);
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  globalShortcut.register("Control+Command+Tab", () =>
+    tray.displayBalloon(notifBalloon)
+  );
 
   tray.setToolTip("vdNameCurrent");
   tray.setContextMenu(contextMenu);
